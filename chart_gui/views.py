39e45e4c9ddde31f00e81b2573ace653bb10846f
xingdawang <wxd598113636@gmail.com>
2015-12-01 23:42:17 +0000

    django framework using google charts with foor loops

---
+++ chart_gui/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import Http404
+
+# Create your views here.
+def index (request):
+	return render(request, 'index2.html', {
+		'test': ['aa', 'bb', 'cc'],
+	})
